093a33d694dae16e98dd8a6c8fa73583eed1ea9d
Andrew MacLean <andrewmaclean@dreambook.lan>
2019-05-07 22:58:38 -0700

    let's see if this works better

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // return types
 
-const exerciseFunctions = [
-    () => {
-        console.log("Hello World")
-    }
-]
+function workingCode() {
+    console.log("Hello World")
+}
 
-exerciseFunctions.forEach( fun => {
-    const retVal = fun()
-    if (retVal !== undefined) console.log(retVal)
-})
+console.log(workingCode())
 
 //
 
