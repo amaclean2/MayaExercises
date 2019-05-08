dd5db252e2516c16bebad3c374b3fe41a8703ad0
Andrew MacLean <andrewmaclean@dreambook.lan>
2019-05-07 22:42:32 -0700

    exercises

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function io() {
-    console.log("Hey there")
-}
+// return types
+
+const exerciseFunctions = [
+    () => {
+        console.log("Hello World")
+    }
+]
+
+exerciseFunctions.forEach( fun => {
+    const retVal = fun()
+    if (retVal !== undefined) console.log(retVal)
+})
+
+//
+
