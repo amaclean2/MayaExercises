306a599e02473bdf213114cd0156bbddc758f2aa
Maya Goody <Maya@mayagoobookpro3.lan>
2019-05-08 00:30:46 -0700

    primes

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // return types
 
+function listOfPrimes (number) {
+    var it = 2
+    var listOfPrimes = []
+    while (it<number){
+        var checkIt = 2
+        var compositeCheck = false
+        while (checkIt < it){
+            if (it % checkIt === 0){
+                compositeCheck = true
+            }
+            checkIt += 1
+        }
+        if (compositeCheck === false){
+            listOfPrimes.push(it)
+        }
+        it += 1
+    }
+    return listOfPrimes
+}
 function workingCode() {
-    console.log("Hello World")
+
+   console.log(listOfPrimes(1000))
 }
 
 workingCode()
